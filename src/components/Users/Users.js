ac57d954dd4d1c7d8d904b135ce9507d9ca0f170
Тимофеева Екатерина Владимировна <TimofeevaEV@pktbl.org.rzd>
2020-02-11 16:48:24 +0300

    rewrite  component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,78 +1,54 @@
 import React from 'react';
 import s from './Users.module.css'; /*elem.location.city}, {elem.location.country}*/
-import * as axios from 'axios';
 
-class Users extends React.Component{
+let Users = (props) => {
 
-componentDidMount(){
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-  axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-  .then(resp => {
-
-    this.props.setUsers(resp.data.items)
-    this.props.setTotalUsersCount(resp.data.totalCount);
-    console.log(resp.data.totalCount)
-  });
-}
-
-onPageChechged = (numPage) => { // стрелочная функция чтобы  сохранить контекст вызова
-	console.log(numPage);
-  this.props.setCurrentPage(numPage);
-
-  axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${numPage}&count=${this.props.pageSize}`)
-  .then(resp => {
-    this.props.setUsers(resp.data.items);
-  /*  this.props.totalUsrsCount(resp.data.totalCount);*/
-  });
-}
-
-  render(){
-
-    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-    let pages = [];
-    for (let i = 0; i < pagesCount; i++) {
-        pages.push(i+1);
-    }
-
-let pagesPagination = pages.map(num => {
-   return <span className={this.props.currentPage === num ? s.selectedPage : ''} onClick={(e) => {this.onPageChechged(num)}} key={num}>
-                 {num}
-           </span>
-});
-
-  /*
-
-  <span className={s.selectedPage}>2</span>
-  <span className>3</span>
-  <span className>4</span>
-  <span className>5</span>
-  */
-
-    return  <div>
-             <div>
-                  {pagesPagination}
-             </div>
-           {
-                this.props.users.map(obj => <div key={obj.id}>
-                        <span>
-                           <img src={obj.photos.small !== null ? obj.photos.small : "https://avatarko.ru/img/kartinka/2/Gubka_Bob.jpg"} />
-                        </span>
-                        <span>
-                        {   obj.followed
-                          ?  <button onClick={() => {this.props.unfollow(obj.id)}}>UnFollow</button>
-                          :  <button onClick={() => {this.props.follow(obj.id)}}>Follow</button>
-                        }
-
-                        </span>
-                        <span>
-                         <span>{obj.name}</span>
-                          <span> {'obj.lacation.city'} , {'obj.lacation.country'} </span>
-                        </span>
-              </div>)
-           }
-       </div>
+  let pages = [];
+  for (let i = 0; i < pagesCount; i++) {
+      pages.push(i+1);
   }
+
+
+  let pagesPagination = pages.map(num => {
+     return <span className={props.currentPage === num ? s.selectedPage : ''} onClick={(e) => {props.onPageChechged(num)}} key={num}>
+                   {num}
+             </span>
+  });
+
+return  <div>
+           <div>
+                  {pagesPagination}
+
+           </div>
+         {
+              props.users.map(obj => <div key={obj.id}>
+                      <span>
+                         <img src={obj.photos.small !== null ? obj.photos.small : "https://avatarko.ru/img/kartinka/2/Gubka_Bob.jpg"} />
+                      </span>
+                      <span>
+                      {   obj.followed
+                        ?  <button onClick={() => {props.unfollow(obj.id)}}>UnFollow</button>
+                        :  <button onClick={() => {props.follow(obj.id)}}>Follow</button>
+                      }
+
+                      </span>
+                      <span>
+                       <span>{obj.name}</span>
+                        <span> {'obj.lacation.city'} , {'obj.lacation.country'} </span>
+                      </span>
+            </div>)
+         }
+     </div>
 }
 
 export default Users;
+
+
+
+/*{pages.map(num => {
+   return <span className={this.props.currentPage === num ? s.selectedPage : ''} onClick={(e) => {this.onPageChechged(num)}} key={num}>
+                 {num}
+           </span>}
+         )}*/
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -2,6 +2,41 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC} from '../../UsersReducer';
 import Users from './Users';
+import * as axios from 'axios';
+import s from './Users.module.css';
+
+
+class UsersContainer extends React.Component{
+
+componentDidMount(){
+
+  axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+  .then(resp => {
+
+    this.props.setUsers(resp.data.items)
+    this.props.setTotalUsersCount(resp.data.totalCount);
+    console.log(resp.data.totalCount)
+  });
+}
+
+onPageChechged = (numPage) => { // стрелочная функция чтобы  сохранить контекст вызова
+	console.log(numPage);
+  this.props.setCurrentPage(numPage);
+
+  axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${numPage}&count=${this.props.pageSize}`)
+  .then(resp => {
+    this.props.setUsers(resp.data.items);
+  });
+}
+
+  render(){
+    return  <Users onPageChechged={this.onPageChechged} totalUsersCount={this.props.totalUsersCount}
+               pageSize={this.props.pageSize} currentPage={this.props.currentPage}
+                   users={this.props.users} unfollow={this.props.unfollow} follow={this.props.follow}/>
+  }
+}
+
+
 
 let mapStateToProps = (state) => {
   return  {
@@ -22,5 +57,4 @@ let mapDispatchToProps = (dispatch) => {
                 }
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
